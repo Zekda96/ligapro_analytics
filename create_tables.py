788b888b81e29748f7b8e8ea2547032cac579b7a
Zekda96 <36642875+Zekda96@users.noreply.github.com>
2024-03-12 03:28:58 +0000

    better table with most passes and most shots

--- create_tables.py
+++ create_tables.py
@@ -52,28 +52,32 @@ def team_name_to_path(fn):
     fp = os.path.join(img_path, f'{teams[fn]}.png')
     return fp
 
-def order_data(by):
-   
-    df = read_db()
-
-    select = ['player',
-            'team',
-            'minutesPlayed',
-            'totalPass',
-            'accuratePass',
-            'TotalShots',
-            'ShotOnTarget'
+def order_data(df, stat_type):
+    select = [
+        'player', 'team', 'minutesPlayed'
     ]
 
-    where = df['player'] == 'Leonar Espinoza'
+    if stat_type == 'totalPass':
+        select.append('totalPass')
+        select.append('accuratePass')
+        select.append('goalAssist')
+
+        order_by = 'totalPass'
+
+    elif stat_type == 'TotalShots':
+        select.append('TotalShots')
+        select.append('ShotOnTarget')
+        select.append('goals')
+
+
+        order_by = 'TotalShots'
+
     group_by = ['team','player']
-    order_by = by
     limit = 10
 
     if order_by not in select:
         select.append(order_by)
 
-    # new_df = new_df[where]
     df = df[select]
     df = df.groupby(group_by).sum()
     df = df.sort_values(order_by, ascending=False)
@@ -87,13 +91,14 @@ def order_data(by):
     df.index += 1
 
     # Add percentages
-    # - Pass Accuracy
-    pass_accuracy = (df['accuratePass'] / df['totalPass'])
-    df.insert(5, 'PassAccuracy', pass_accuracy)
-
-    # - Shot on Target %
-    shot_on_target = (df['ShotOnTarget'] / df['TotalShots'])
-    df.insert(len(df.columns), 'SoT_percent', shot_on_target)
+    if stat_type == 'totalPass':
+        # - Pass Accuracy
+        pass_accuracy = (df['accuratePass'] / df['totalPass'])
+        df.insert(len(df.columns)-1, 'PassAccuracy', pass_accuracy)
+    elif stat_type == 'TotalShots':
+        # - Shot on Target %
+        shot_on_target = (df['ShotOnTarget'] / df['TotalShots'])
+        df.insert(len(df.columns)-1, 'SoT_percent', shot_on_target)
 
     # Replace NaNs with 0
     df = df.fillna(0)
@@ -101,160 +106,196 @@ def order_data(by):
     # Change team names to logo images filepaths for plottable
     df['team'] = df['team'].apply(team_name_to_path)
 
+    df['minutesPlayed'] = df['minutesPlayed'] / 90
+
     return df
 
-def plot_table(df, by, output_fn):
-    cmap = LinearSegmentedColormap.from_list(
-        name="bugw", colors=["#ffffff", "#f2fbd2", "#c9ecb4", "#93d3ab", "#35b0ab"], N=256
-    )
+def plot_table(df, stat_type, output_fn):
 
+    # --------------------------------------- Figure
+    bg_color = '#faf9f4'
+
+     # Table
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(11, 8))
+    ax.set_facecolor(bg_color)
+
+    # Axis for title
+    h = 0.1
+    ax_title = fig.add_axes([0, 0.9, 1, h], zorder=1)
+    ax_title.axis('off')
+
+    col_defs = get_col_defs(stat_type)
+
+    tab = Table(df,
+                ax=ax,
+                column_definitions=col_defs,
+                textprops={
+                    "ha": "center",
+                    },
+                )
+    
+    if stat_type=='shot':
+        fig.suptitle('Top 10 Jugadores con más disparos', fontsize=17)
+    elif stat_type=='pass':
+        fig.suptitle('Top 10 Jugadores con más pases', fontsize=17)
+
+    ax.set_title('LigaPro 2024 - 2 Fechas', fontsize=15)
+
+    # -- Transformation functions (thanks Son of a corner)
+    DC_to_FC = ax_title.transData.transform
+    FC_to_NFC = fig.transFigure.inverted().transform
+    # Transform for title axes
+    ax_title_tf = lambda x: FC_to_NFC(DC_to_FC(x))
+
+    # Add team logo
+    ax_coords = ax_title_tf((0.205, 0.1))
+    ax_size = 0.155
+    image = Image.open(os.path.join('data', 'logos', 'lp.png'))
+    newax = fig.add_axes(
+        [ax_coords[0]-ax_size/2, ax_coords[1]-ax_size/2, ax_size, ax_size],
+        anchor='W', zorder=1
+    )
+    newax.imshow(image)
+    newax.axis('off')
+
+    fig.savefig(f"images/{output_fn}", facecolor=ax.get_facecolor(), dpi=200)
+
+
+def get_col_defs(stat_type):
+       
     cmap_test = LinearSegmentedColormap.from_list(
         # name="bugw", colors=["#ffffff", "#f2fbd2", "#c9ecb4", "#93d3ab", "#35b0ab"], N=256
         name="bugw", colors=["#FF9C9C", "#FFFFFF", "#53CD76"], N=256
     )
 
     cmap1 = LinearSegmentedColormap.from_list(
-        name="gray", colors=[
+        name="gray",
+        colors=[
             "#f2f2f4",
             "#f2f2f4",
             ], N=256
     )
 
-    percents_cmap = matplotlib.colormaps.get_cmap('RdYlGn')
-
     data_width = 0.6
     percent_width = 1
-    col_defs = (
-        [
-            ColumnDefinition(
+
+    col_defs = [
+        ColumnDefinition(
             name='index',
             title='',
             width=0.3,
             ),
 
+        ColumnDefinition(
+            name='player',
+            title='',
+            width=1.5,
+            ),
 
-            ColumnDefinition(
-                name='player',
-                title='',
-                width=1.8,
-                ),
+        ColumnDefinition(
+            name='team',
+            title='',
+            plot_fn=image,
+            width=1,
+            ),
 
-            ColumnDefinition(
-                name='team',
-                title='',
-                plot_fn=image,
-                width=1,
-                # border='left',
-                ),
+        ColumnDefinition(
+            name='minutesPlayed',
+            title='90s',
+            formatter="{:.1f}",
+            width=data_width,
+            )
+        ]
 
-            ColumnDefinition(
-                name='minutesPlayed',
-                title='Minutos',
-                formatter="{:.0f}",
-                width=data_width,
-                # border='left',
-                ),
+    if stat_type == 'pass':
+        stats_col_defs = [
+        ColumnDefinition(
+            name='totalPass',
+            title='Total',
+            formatter="{:.0f}",
+            group="Pases",
+            width=data_width,
+            cmap=cmap1,
+            ),  
 
-            ColumnDefinition(
-                name='totalPass',
-                title='Pases',
-                formatter="{:.0f}",
-                group="Pases",
-                width=data_width,
-                # border='left',
-                ),
+        ColumnDefinition(
+            name='accuratePass',
+            title='Completos',
+            formatter="{:.0f}",
+            group="Pases",
+            width=data_width,
+            ),
 
-            ColumnDefinition(
-                name='accuratePass',
-                title='Completos',
-                formatter="{:.0f}",
-                group="Pases",
-                width=data_width,
-                # border='left',
-                ),
+        ColumnDefinition(
+            name='PassAccuracy',
+            title='Precisión',
+            formatter=decimal_to_percent,
+            group="Pases",
+            cmap=cmap_test,
+            width=percent_width,            
+            textprops= {"bbox": {"boxstyle": "circle", "pad": 0.05}},
+            ),
 
+        ColumnDefinition(
+            name='goalAssist',
+            title='Asistencias',
+            formatter="{:.0f}",
+            group="Pases",
+            width=data_width,
+            ) 
+        ] 
 
-            ColumnDefinition(
-                name='PassAccuracy',
-                title='Precisión',
-                formatter=decimal_to_percent,
-                group="Pases",
-                cmap=cmap_test,
-                width=percent_width,            
-                textprops= {"bbox": {"boxstyle": "circle", "pad": 0.35}},
-                # border='left',
-                ),
-
+            
+    elif stat_type == 'shot':
+        stats_col_defs = [
             ColumnDefinition(
                 name='TotalShots',
                 title='Total',
                 formatter="{:.0f}",
-                group="Disparos",
+                group="Tiros",
                 width=data_width,
-                # cmap=cmap1,
-                # border='left',
+                cmap=cmap1,
                 ),
 
             ColumnDefinition(
                 name='ShotOnTarget',
                 title='Al Arco',
                 formatter="{:.0f}",
-                group="Disparos",
+                group="Tiros",
                 width=data_width,
-                # border='left',
                 ),
 
             ColumnDefinition(
                 name='SoT_percent',
                 title='Precisión',
                 formatter=decimal_to_percent,
-                group="Disparos",
+                group="Tiros",
                 cmap=cmap_test,
-                textprops= {"bbox": {"boxstyle": "circle", "pad": 0.35}},
+                textprops= {"bbox": {"boxstyle": "circle", "pad": 0.05}},
                 width=percent_width,
                 ),
-        ] 
-        + [
+
             ColumnDefinition(
-                name=by,
-                cmap=cmap1,
-                )
+                    name='goals',
+                    title='Goles',
+                    formatter="{:.0f}",
+                    group="Tiros",
+                    width=data_width,
+                    ) 
         ]
-    )
 
-    # col_defs.
+    for stat in stats_col_defs:
+        col_defs.append(stat)
 
-
-    # Table
-    fig, ax = plt.subplots(figsize=(13, 8))
-    
-    bg_color = '#faf9f4'
-    # fig.patch.set_facecolor('red')
-    ax.set_facecolor(bg_color)
-
-    tab = Table(df,
-                column_definitions=col_defs,
-                # cell_kw={'facecolor': 'red'}
-                textprops={
-                    "ha": "center",
-                    },
-                )
-
-    fig.suptitle('Top 10 Jugadores con mas disparos', fontsize=17)
-    ax.set_title('LigaPro 2024 - 2 Fechas')
-
-    fig.savefig(f"images/{output_fn}", facecolor=ax.get_facecolor(), dpi=200)
-
+    return col_defs    
 
 if __name__ == "__main__":
 
-    top_stat = 'TotalShots'
-    filename = 'total_shots.png'
-    df = order_data(top_stat)
-    plot_table(df, top_stat, filename)
+    df = read_db()
 
-    top_stat = 'totalPass'
-    filename = 'total_passes.png'
-    df = order_data(top_stat)
-    plot_table(df, top_stat, filename)
+    # --------------------------------------- Add Tables
+    df_pass = order_data(df, 'totalPass')
+    plot_table(df_pass, 'pass', 'total_passes.png')
 
+    df_shot = order_data(df, 'TotalShots')
+    plot_table(df_shot, 'shot', 'total_shots.png')
